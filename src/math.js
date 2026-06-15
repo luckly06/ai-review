0b99ab271de3465c2249ff9b32ffbe421d850123
何億成 <425360154@qq.com>
2026-06-15 10:20:49 +0800

    feat(math): add type check + multiply

--- src/math.js
+++ src/math.js
@@ -1,7 +1,14 @@
 export function add(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('add 只接受数字');
+  }
   return a + b;
 }
 
 export function subtract(a, b) {
   return a - b;
+}
+
+export function multiply(a, b) {
+  return a * b;
 }
